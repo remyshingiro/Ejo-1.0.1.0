5b86582fc877528cd629e21ba07b25a2f20f321e
Remy <shingiroremy303@gmail.com>
2026-03-28 18:53:17 +0200

    fix: deploy issues solved

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { auth, db } from '../firebase/config';
 import { doc, onSnapshot } from 'firebase/firestore';
 import Layout from '../components/Layout';
--- src/pages/Savings.tsx
+++ src/pages/Savings.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import { auth, db } from '../firebase/config';
 import { doc, onSnapshot } from 'firebase/firestore';
